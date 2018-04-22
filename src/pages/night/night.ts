c6cb7e51d447f51eefd04973425b531bed522bc8
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-04-22 11:11:04 +0200

    Added about and night settings

--- src/pages/night/night.ts
+++ src/pages/night/night.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the NightPage page.
@@ -9,16 +9,36 @@ import { NavController, NavParams } from 'ionic-angular';
  */
 
 @Component({
-  selector: 'page-night',
-  templateUrl: 'night.html',
+    selector: 'page-night',
+    templateUrl: 'night.html',
 })
 export class NightPage {
+    iso: number = 0;
+    aperture: number = 0;
+    apertureValues = ["f2", "f2.8", "f4", "f5.6", "f8", "f11", "f16", "f22"];
+    isoValues = ["100", "200", "400", "800", "1600", "3200"];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    nightShutterspeed = [
+        ["6s  ", "15s ", "36s  ", "1m30s  ", "3m30s", "8m50s", "21m30s", "52m50s"],
+        ["2.5s", "6s  ", "15s  ", "  36s  ", "1m30s", "3m30s", "8m50s", "21m30s"],
+        ["1s  ", "2.5s", " 6s  ", "  15s  ", "36s", "1m30s", "3m30s", "8m50s"],
+        ["1/2s", "1s  ", " 2.5s", "   6s  ", "15s", "36s", "1m30s", "3m30s"],
+        ["1/4s", "1/2s", " 1s  ", "   2.5s", "6s", "15s", "36s", "1m30s"],
+        ["1/8s", "1/4s", " 1/2s", "   1s  ", "2.5s", "6s", "15s", "36s"]
+    ];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NightPage');
-  }
+    shutter = this.nightShutterspeed[0][0];
+    
+    isoValue() {
+        return this.isoValues[this.iso];
+    }
+
+    apertureValue() {
+        return this.apertureValues[this.aperture];
+    }
+    recalcShutter() {
+        this.shutter = this.nightShutterspeed[this.iso][this.aperture];
+    }
+
 
 }
